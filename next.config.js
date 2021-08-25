88081844b1f21ea63b0a5754bbd9677f1037e8b7
DeepCode <viacart@gmail.com>
2021-08-25 15:25:05 +0200

    First commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  images: {
+    domains: ['upload.wikimedia.org', 'ngrx.io', 'seeklogo.com', 'www.grafoadv.it', 'image.flaticon.com', 'www.reviviscar.it', 'www.stdout.it'],
+  },
 }
